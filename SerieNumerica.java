624e49faa7b54645cbf5f600fe82a86fd0a1006b
MiguelTorresss <116587393+MiguelTorresss@users.noreply.github.com>
2023-10-29 16:29:49 -0500

    Add files via upload

---
+++ SerieNumerica.java
@@ -0,0 +1,10 @@
+public class SerieNumerica {
+    public static void main(String[] args) {
+        int[] numeros = {1, 2, 5, 14, 7, 20, 13, 6, 17, 10, 29, 22, 15}; // Agrega los números en el orden dado
+
+        System.out.println("Secuencia numérica: ");
+        for (int numero : numeros) {
+            System.out.print(numero + ", ");
+        }
+    }
+}
